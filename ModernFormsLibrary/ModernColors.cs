af82ead4d3880a7fea6be324814ddd33a2150b0c
Cory Borrow <cborrow03@gmail.com>
2015-01-13 00:27:51 -0500

    Changed color names.
    Changed the names of a couple of colors to be the same name as where
    they are used in there controls.

--- ModernFormsLibrary/ModernColors.cs
+++ ModernFormsLibrary/ModernColors.cs
@@ -8,18 +8,11 @@ namespace ModernForms
 {
     public static class ModernColors
     {
-        static Color foreColor = Color.Black;
+        static Color foreColor = Color.FromArgb(31, 31, 31);
         public static Color ForeColor
         {
             get { return foreColor; }
             set { foreColor = value; }
-        }
-
-        static Color pressedForeColor = Color.White;
-        public static Color PressedForeColor
-        {
-            get { return pressedForeColor; }
-            set { pressedForeColor = value; }
         }
 
         static Color backColor = Color.FromArgb(243, 243, 243);
@@ -36,18 +29,32 @@ namespace ModernForms
             set { borderColor = value; }
         }
 
-        static Color selectedBackColor = Color.FromArgb(221, 221, 221);
-        public static Color SelectedBackColor
+        static Color hotTrackColor = Color.FromArgb(221, 221, 221);
+        public static Color HotTrackColor
         {
-            get { return selectedBackColor; }
-            set { selectedBackColor = value; }
+            get { return hotTrackColor; }
+            set { hotTrackColor = value; }
         }
 
-        static Color accentColor = Color.FromArgb(42, 115, 234);
+        static Color accentColor = Color.FromArgb(51, 102, 255);
         public static Color AccentColor
         {
             get { return accentColor; }
             set { accentColor = value; }
+        }
+
+        static Color pressedForeColor = Color.White;
+        public static Color PressedForeColor
+        {
+            get { return pressedForeColor; }
+            set { pressedForeColor = value; }
+        }
+
+        static Color pressedBackColor = accentColor;
+        public static Color PressedBackColor
+        {
+            get { return pressedBackColor; }
+            set { pressedBackColor = value; }
         }
     }
 }
